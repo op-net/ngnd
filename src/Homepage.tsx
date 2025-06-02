187199d8ccce2c836a1013d3b543ab20418a529f
Nel <01169201@pw.edu.pl>
2025-06-02 21:09:36 +0200

    Starting template for the website

---
+++ src/Homepage.tsx
@@ -0,0 +1,52 @@
+import { Box, Typography, Drawer, List, ListItem, ListItemButton, ListItemText, Button } from "@mui/material";
+import logo from './main_icon.png';
+import { useNavigate } from "react-router-dom";
+import { homepage_url } from "./constants";
+
+
+const drawerWidth = 200;
+
+const HomePage = () => {
+  let navigate = useNavigate();
+
+  return (
+    <Box sx={{ display: "flex", minHeight: "100vh" }}>
+      {/* Sidebar */}
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: "border-box", backgroundColor: "#f5f5f5" },
+        }}
+      >
+        <Box sx={{ p: 2, textAlign: "center" }}>
+          <Button onClick={() => navigate(homepage_url)} sx={{ borderRadius: "50%" }}>
+            <img src={logo} alt="Logo" style={{ width: 50, marginBottom: 10 }} />
+          </Button>
+        </Box>
+        <List>
+          {["Networks", "Contributors", "Privacy Policy"].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      {/* Main content */}
+      <Box component="main" sx={{ flexGrow: 1, p: 4 }}>
+        <Typography variant="h3" gutterBottom>
+          Welcome to SND extension lib
+        </Typography>
+        <Typography variant="body1" color="text.secondary">
+          (TODO text)
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default HomePage;
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+const homepage_url = '/snd-extension-lib/';
+const networks_url = '/snd-extension-lib/networks';
+
+export { homepage_url, networks_url };
